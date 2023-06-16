ff90d4cdff9c1a543ba5fc8df7baa8aaab151d88
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-06-16 20:46:39 +0530

    Update Check Prime.cpp

--- Basic Maths Concepts/Check Prime.cpp
+++ Basic Maths Concepts/Check Prime.cpp
@@ -1,25 +1,31 @@
 // Check if number is prime or not
 
 #include<bits/stdc++.h>
-  using namespace std;
-  int n;
-  cin >> n;
-  int cnt = 0;
-  for (int i = 2; i < n; ++i)
-    {
-      if (n % i == 0)
-	{
-	  cnt++;
-	  break;
-	}
-    }
-  if (cnt == 0)
-    {
-      cout << "Prime Number" << endl;
-    }
-  else
-    {
-      cout << "Not Prime" << endl;
-    }
-  return 0;
+using namespace std;
+int main()
+{
+int n;
+cin >> n;
+int cnt = 0;
+if(n==1)
+{
+    cout<<"1 is neither prime nor composite";
+}
+for(int i = 2; i < n; ++i)    // Time complexity -> 0(n)
+{
+if(n % i == 0){
+cnt++;
+break;
+}
+}
+
+if(cnt == 0 && n!=1)
+{
+cout << "Prime Number" << endl;
+}
+if(cnt != 0 && n!=1)
+{
+cout << "Not Prime Number" << endl;
+}
+return 0;
 }
