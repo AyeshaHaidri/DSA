c935839f8ae7f4fa1cd6135ce6ea2c603cdb0ab9
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-06-23 23:45:32 +0530

    Create Max element.cpp

---
+++ Array/Max element.cpp
@@ -0,0 +1,38 @@
+// Maximum element in array
+
+// Brute Force Approach
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int arr[] = {67,23,14,56,90,11};
+int n=sizeof(arr)/sizeof(arr[0]);
+sort(arr,arr+n);
+cout << "Maximum element is: "<<arr[n-1] << endl;
+return 0;
+}
+
+// Time complexity -> O(nlogn)
+
+// Optimized Approach
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int arr[] = {67,23,14,56,90,11};
+int n=sizeof(arr)/sizeof(arr[0]);
+int max = arr[0];
+for(int i = 1; i < n; ++i)
+{
+if(arr[i] > max)
+{
+max = arr[i];
+}
+}
+cout << "Maximum element is: "<< max << endl;
+return 0;
+}
+
+// Time complexity -> O(n)
