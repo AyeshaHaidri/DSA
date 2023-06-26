16d9642a5d8ea09cf937d40ce5d1bc10ecf04c29
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-06-26 23:36:33 +0530

    Create Second Largest.cpp

---
+++ Array/Second Largest.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int arr[] = {1,2,4,8,8,9,10,678};
+int n = 8;
+
+int largest = arr[0];
+for(int i = 0; i < n; ++i)
+{
+if(arr[i] > largest)
+{
+largest = arr[i];
+}
+}
+int sl = -1;
+for(int i = 0; i < n; ++i)
+{
+if(arr[i] > sl && arr[i] != largest)
+{
+sl = arr[i];
+}
+}
+cout << sl << endl;
+}
