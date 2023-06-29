f07d38e36287ef6768046f2d0d7cd2142a9159fd
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-06-29 23:51:05 +0530

    Update Factorial.cpp

--- Recursion/Factorial.cpp
+++ Recursion/Factorial.cpp
@@ -1,1 +1,25 @@
+// Factorial of a positive number using Recursion
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n);
+
+int main() {
+
+  int n;
+  cin >> n;
+  cout << factorial(n);
+  return 0;
+}
+
+int factorial(int n) {
+  if(n > 1)
+  {
+    return n * factorial(n - 1);
+  }
+  else
+  {
+    return 1;
+  }
+}
