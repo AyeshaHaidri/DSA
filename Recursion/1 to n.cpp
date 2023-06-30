b33f64bb4ec7faf068af9093ba93a61778c3118b
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-06-30 23:39:23 +0530

    Create 1 to n.cpp

---
+++ Recursion/1 to n.cpp
@@ -0,0 +1,20 @@
+// Print numbers from 1 to n using recursion
+
+#include<bits/stdc++.h>
+using namespace std;
+void print(int n){
+if(n==0)
+{
+return;
+}
+
+cout << n << endl;
+print(n-1);
+}
+int main()
+{
+    int a;
+    cin>>a;
+    print(a);
+    return 0;
+}
