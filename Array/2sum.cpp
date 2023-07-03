2b0f2ad63b3b988a76b779870867fcea7123fcea
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-07-03 23:46:03 +0530

    Update 2sum.cpp

--- Array/2sum.cpp
+++ Array/2sum.cpp
@@ -39,3 +39,34 @@ return 0;
 
 // T.C => O(n^2)
 // S.c => O(1)
+
+// Optimal Approach
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+map<int,int> m;
+
+int arr[] = {2,5,1,8,9};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  int target = 9;
+
+for(int i = 0; i < n; ++i)        // O(n)
+{
+int num = arr[i];
+int required = target - num;
+
+if(m.find(required) != m.end())   // O(logn)
+{  
+cout << required << " " << num;
+break;
+}
+m[num] = i;
+}
+return 0;
+}
+
+// T.C => O(n^2) => worst case(unordered_map)
+// T.C => O(nlogn)
+// S.C => O(N)
