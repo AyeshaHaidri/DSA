739e42817b4890edfbd62afbbca8c211097df0ec
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-07-04 22:53:17 +0530

    Update Sort 0's,1's & 2's.cpp

--- Array/Sort 0's,1's & 2's.cpp
+++ Array/Sort 0's,1's & 2's.cpp
@@ -20,3 +20,55 @@ return 0;
 }
 // T.C => O(nlogn)
 // S.C => O(1)
+
+
+// optimal solution
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int arr[] = {0,1,2,0,1,2,1,2,0,0,0,1};
+int n = sizeof(arr)/sizeof(arr[0]);
+
+int cnt_zero = 0;
+int cnt_one = 0;
+int cnt_two = 0;
+
+for(int i = 0; i < n; ++i)  // O(n)
+{
+if(arr[i] == 0)
+{
+cnt_zero++;
+}else if(arr[i] == 1)
+{
+cnt_one++;
+}else if(arr[i] == 2)
+{
+cnt_two++;
+}
+}
+
+for(int i = 0;i < cnt_zero; ++i)  // O(1/3)
+{
+arr[i] = 0;
+}
+
+for(int i = cnt_zero; i < cnt_zero + cnt_one; ++i) // O(2/3)
+{
+arr[i] = 1;
+}
+
+for(int i = cnt_zero + cnt_one; i < n; ++i) // O(3/3)
+{
+arr[i] = 2;
+}
+
+for(int e : arr)
+{
+cout << e << " ";
+}
+return 0;
+}
+
+// T.C => O(n + n) => O(2n) => O(n)
+// S.C => O(1)
