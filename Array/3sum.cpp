c762749759db972b166d2e4637719a18d062ce09
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-07-05 22:55:39 +0530

    Update 3sum.cpp

--- Array/3sum.cpp
+++ Array/3sum.cpp
@@ -90,3 +90,68 @@ int main()
     return 0;
 }
 
+
+// Optimal Approach
+
+// 2 Pointers Approach
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[] = {-1,0,1,2,-1,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    sort(arr,arr+n);
+
+    vector<int> temp;
+    vector<vector<int>> ans;
+
+    for(int i = 0; i < n; ++i)  // O(n)
+    {
+        if(i > 0 && arr[i] == arr[i-1]){
+            continue;
+        }
+        int j = i + 1;
+        int k = n - 1;
+        while(j < k)  // O(n)
+        {
+            int sum = arr[i] + arr[j] + arr[k];
+            if(sum < 0){
+                j++;
+            }else if(sum > 0)
+            {
+                k--;
+            }else{
+                temp.push_back(arr[i]);
+                temp.push_back(arr[j]);
+                temp.push_back(arr[k]);
+                ans.push_back(temp);
+                temp.clear();
+                j++;
+                k--;
+                while(j < k && arr[j] == arr[j-1]){
+                    j++;
+                }
+
+                while(j < k && arr[k] == arr[k+1]){
+                    k--;
+                }
+            }
+
+        }
+    }
+
+    for(auto triplet : ans)
+    {
+        for(auto k : triplet){
+            cout << k << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+    // T.C => O(n^2) + nlogn
+    // S.C => (one triplet)
+
+
