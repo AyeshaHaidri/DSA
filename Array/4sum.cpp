cadc8dba6c118f40b70036e5cd2bdede5576e8be
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-07-06 23:33:27 +0530

    Update 4sum.cpp

--- Array/4sum.cpp
+++ Array/4sum.cpp
@@ -51,3 +51,54 @@ int main()
     }
     return 0;
 }
+
+
+// Optimal Approach
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[] = {1,0,-1,0,-2,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int target = 0;
+
+    vector<int> temp;
+    set<vector<int>> st;
+
+    for(int i = 0; i < n; ++i) // O(n)
+    {
+        for(int j = i + 1; j < n; ++j) // O(n)
+        {
+            set<int> s;
+            for(int k = j + 1; k < n; ++k)
+            {
+                int fourth = target -(arr[i] + arr[j] + arr[k]);
+                if(s.find(fourth) != s.end()){  
+                temp.push_back(arr[i]);
+                temp.push_back(arr[j]);
+                temp.push_back(arr[k]);
+                temp.push_back(fourth);
+                sort(temp.begin(),temp.end());
+                st.insert(temp);
+                temp.clear();
+            }
+            s.insert(arr[k]);  
+            }
+        }
+    }
+
+    for(auto it : st)
+    {
+        for(auto k : it)
+        {
+            cout << k << " ";
+        }
+        cout << endl;
+    }
+
+    // T.C => O(n^nlogn) => O(n^3)
+    // S.C => O(no. of quadruplets) * 2 + O(k)
+    return 0;
+}
+
+
