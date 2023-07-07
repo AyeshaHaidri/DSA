95f60f2f7113bfb664778e8b3993b38e72e60dc8
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-07-07 16:33:01 +0530

    Update Second Largest.cpp

--- Array/Second Largest.cpp
+++ Array/Second Largest.cpp
@@ -28,3 +28,31 @@ cout << sl << endl;
 
 // T.C => n + n => O(2n)
 // S.C => O(1)
+
+// optimized approach
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int arr[] = { 9, 2, 4, 7, 7, 5 };
+  int n = 6;
+  int largest = arr[0];
+  int sl = -1;
+
+  for (int i = 1; i < n; ++i)
+    {
+      if (arr[i] > largest)
+	{
+	  sl = largest;
+	  largest = arr[i];
+	}
+      else if (arr[i] < largest && arr[i] > sl)
+	{
+	  sl = arr[i];
+	}
+    }
+  cout << sl << endl;
+  return 0;
+}
+
+// T.C => o(n)
