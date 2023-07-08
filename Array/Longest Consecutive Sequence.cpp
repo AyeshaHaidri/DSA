e70fa05affc1315451ef3bfe2e7c0b901437d618
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-07-08 23:56:41 +0530

    Create Longest Consecutive Sequence.cpp

---
+++ Array/Longest Consecutive Sequence.cpp
@@ -0,0 +1,43 @@
+// longest consecutive sequence
+
+// Brute Force
+
+#include<bits/stdc++.h>
+using namespace std;
+bool linearSearch(int arr[],int n,int target) // O(n)
+{
+    for(int i = 0; i < n; ++i)
+    {
+        if(arr[i] == target)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+int main()
+{
+    int arr[] = {102,4,100,1,101,3,2,103,104};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int longest = 1;
+
+    for(int i = 0; i < n; ++i)  // O(n)
+    {
+        int x = arr[i];
+        int cnt = 1;
+        while(linearSearch(arr,n,x+1) == true){
+            x = x+1;
+            cnt++;
+
+            longest = max(longest,cnt);
+        }
+    }
+
+    cout << longest  << endl;
+    return 0;
+}
+
+// T.C => O(n^2)
+// S.C => O(1)
