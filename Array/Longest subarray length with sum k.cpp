fb2bcfb155d91b309077ca1c9e82adb0c83674e1
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-07-10 22:45:41 +0530

    Update Longest subarray length with sum k.cpp

--- Array/Longest subarray length with sum k.cpp
+++ Array/Longest subarray length with sum k.cpp
@@ -36,3 +36,44 @@ int main()
     // S.C => O(1)
 
 
+// Optimal Solution
+// Two Pointers Approach
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[] = {1,2,3,1,1,1,3,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int k = 6;
+    int left = 0;
+    int right = 0;
+    int sum = arr[0];
+    int len = 0;
+
+    while(right < n)  // O(n)
+    {
+        while(left <= right && sum > k) // overall O(n)
+        {
+            sum-=arr[left];
+            left++;
+        }
+        if(sum == k)
+        {
+            len = max(len,right - left + 1);
+        }
+        right++;
+        if(right < n)
+        {
+            sum+=arr[right];
+        }
+    }
+    cout << len << endl;
+    return 0;
+}
+
+ // T.C => O(2N)
+ // S.C => O(1)
+
+
+
