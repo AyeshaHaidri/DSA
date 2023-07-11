58d43ea280b7acad70d6ce46a05c05289a5d4144
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-07-11 23:30:38 +0530

    Update Maximum subarray sum.cpp

--- Array/Maximum subarray sum.cpp
+++ Array/Maximum subarray sum.cpp
@@ -25,3 +25,40 @@ int main()
     // T.C => O(n^2)
     // S.C => O(1)
 
+
+// kadane's algo
+// arr[] = {-2,-3,4,-1,-2,1,5-3}
+
+// int maxi = INT_MIN => -2 => 4 => 7
+// int sum = 0 => -2 => 0 => -3 => 0 => 4 => 3 => 1 => 2 => 7 => 4
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[] = {-2,-3,4,-1,-2,1,5,-3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int sum = 0;
+    int maxi = INT_MIN;
+
+    for(int i = 0; i < n; ++i) // O(n)
+    {
+        sum+=arr[i];
+
+        if(sum > maxi)
+        {
+            maxi = sum;
+        }
+
+        if(sum < 0)
+        {
+            sum = 0;
+        }
+    }
+    cout << maxi << endl;
+    return 0;
+}
+
+    // T.C => O(n)
+    // S.C => O(1)
