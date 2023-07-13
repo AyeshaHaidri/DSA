d968c7d2b3dc7fb936e1fe623b01683aaa3151cc
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-07-13 23:38:04 +0530

    Create Rotate matrix by 90 degrees.cpp

---
+++ Array/Rotate matrix by 90 degrees.cpp
@@ -0,0 +1,47 @@
+//Brute Force
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> rotate(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+
+    vector<vector<int>> ans(n,vector<int>(n,0));
+
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j < n; ++j)
+        {
+            ans[j][n-i-1] = matrix[i][j];
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+        {13,14,15,16}
+    };
+
+
+    vector<vector<int>> ans = rotate(matrix);
+
+    for(auto it : ans)
+    {
+        for(int ele : it)
+        {
+            cout << ele << " ";
+        }
+        cout << endl;
+    }
+
+    // T.C => O(n^2)
+    // S.C => O(n^2)
+    return 0;
+}
