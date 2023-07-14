46852ec1282eb21beadcb14c0228410710bb0624
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-07-14 23:45:22 +0530

    Update Rotate matrix by 90 degrees.cpp

--- Array/Rotate matrix by 90 degrees.cpp
+++ Array/Rotate matrix by 90 degrees.cpp
@@ -46,3 +46,56 @@ int main()
 
  // T.C => O(n^2)
  // S.C => O(n^2)
+
+
+// Optimal Solution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<vector<int>> &matrix){
+    int n = matrix.size();
+
+    for(int i = 0; i < n - 1; ++i)  // O(n)
+    {
+        for(int j = i + 1; j < n; ++j)  // O(n)
+        {
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+
+    for(int i = 0; i < n; ++i)  // O(n)
+    {
+        reverse(matrix[i].begin(),matrix[i].end()); // O(n/2)
+    }
+
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+        {13,14,15,16}
+    };
+
+
+    rotate(matrix);
+
+    for(auto it : matrix)
+    {
+        for(int ele : it)
+        {
+            cout << ele << " ";
+        }
+        cout << endl;
+    }
+
+    // T.C => O(n*n) + O(n*n/2) => O(n^2)
+    // S.C => O(1)
+
+    return 0;
+}
+
+
