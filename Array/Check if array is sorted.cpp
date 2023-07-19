fa5b5aaa31e9fc6eab5c9bdbbd9f439d18b410bf
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-07-19 22:26:05 +0530

    Update Check if array is sorted.cpp

--- Array/Check if array is sorted.cpp
+++ Array/Check if array is sorted.cpp
@@ -2,11 +2,13 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
 int arr[] = {1,2,3,4,5,8,6};
 int n=sizeof(arr)/sizeof(arr[0]);
 int c = 0;
+   
 for(int i = 1; i < n; ++i)
 {
 if(arr[i] >= arr[i-1])
