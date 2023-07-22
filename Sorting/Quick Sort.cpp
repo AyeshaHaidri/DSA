cd46ce2c9b7eabd219d024dbd7b51dd01058d175
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-07-22 23:31:28 +0530

    Update Quick Sort.cpp

--- Sorting/Quick Sort.cpp
+++ Sorting/Quick Sort.cpp
@@ -7,25 +7,21 @@ int findPartitionIndex(int* arr,int low,int high)
 int pivot = arr[low];
 int i = low;
 int j = high;
-
 while(i < j)
 {
 while(arr[i] <= pivot && i <= high - 1)
 {
 i++;
 }
-
 while(arr[j] > pivot && j >= low + 1)
 {
 j--;
 }
-
 if(i < j)
 {
 swap(arr[i],arr[j]);
 }
 }
-
 swap(arr[low],arr[j]);
 return j;
 }
