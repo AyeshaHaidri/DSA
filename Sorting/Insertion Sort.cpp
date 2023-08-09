98f4be521c708ddc686b92a3bdd5f8232bcc5fd1
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-08-09 21:51:53 +0530

    Update Insertion Sort.cpp

--- Sorting/Insertion Sort.cpp
+++ Sorting/Insertion Sort.cpp
@@ -8,9 +8,10 @@ int main()
     int n=sizeof(arr)/sizeof(arr[0]);
     for (int i = 0; i < n - 1; ++i)
    {
+	   int temp =arr[i];
     for (int j = i + 1; j > 0; --j)
       {
-	if (arr[j] < arr[j - 1])
+	if (arr[j] < temp)
 	  {
 	    arr[j+1]=arr[j];
 	  }
