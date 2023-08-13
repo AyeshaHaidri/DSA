93ac2b170720283bbe1b06ab4c63f51f78b8ee04
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-08-13 16:56:00 +0530

    Update Count Sort.cpp

--- Sorting/Count Sort.cpp
+++ Sorting/Count Sort.cpp
@@ -1,32 +1,40 @@
+// count sort -> does not compare elements rather follows indexing,counting and cumulative addition method
+
 #include<bits/stdc++.h>
 using namespace std;
+
 void Countsort(int *input_array,int size,int range)
 {
      int count_array[range];
      int output_array[size];
+	
     //make an empty array of range size
-    for(int i=0;i<range;++i)
+    for(int i=0;i<range;++i)      //O(range)
     {
-        count_array[i]=0;
+        count_array[i]=0;   
     }
+	
     //count the occurrence if each element
-    for(int i=0;i<size;++i)
+    for(int i=0;i<size;++i)       //O(size)
     {
         ++count_array[input_array[i]];
     }
+	
     //cumulative addition of each element of the array
-    for(int i=1;i<range;++i)
+    for(int i=1;i<range;++i)      //O(range)
     {
         count_array[i]=count_array[i]+count_array[i-1];
     }
+	
     // placing input array elements into output array in proper
-	//  positions such that the result is a sorted array in ASC order
-    for(int i=0;i<size;++i)
+    //  positions such that the result is a sorted array in ASC order
+    for(int i=0;i<size;++i)        //O(size)
     {
         output_array[--count_array[input_array[i]]]=input_array[i];
     }
-    //copying data
-    for(int i=0;i<size;++i)
+	
+    //copying elements of sorted array
+    for(int i=0;i<size;++i)       //O(size)
     {
         input_array[i]=output_array[i];
     }
