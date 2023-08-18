018d8e4f2c7682925f40f9e2af23e90e26950eb3
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-08-18 22:39:42 +0530

    Update Bubble Sort.cpp

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -28,15 +28,17 @@ int main()
 // Time Complexity -> O(n^2)
 // Space Complexity -> O(1)
 
+
+// Optimised Approach
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int arr[] = {9,12,3,1,987,0,777,43};
+     int arr[] = {9,12,3,1,987,0,777,43};
      int n=sizeof(arr)/sizeof(arr[0]);
-    for(int i=0;i<n-1;++i)
-        bool swapped=false;
-    {
+     bool swapped=false;
+     for(int i=0;i<n-1;++i)
+     {
         for(int j=0;j<n-i-1;++j)
         {
             if(arr[j]>arr[j+1])
@@ -56,3 +58,8 @@ int main()
     }
     return 0;
 }
+
+// T.C => worst case => O(n^2) 
+// T.C => best case => O(n) ,when the array is already sorted
+// S.C => O(1)
+
