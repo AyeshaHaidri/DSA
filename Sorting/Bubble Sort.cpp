3271f8678cc5cecb911de3c3ac8aa215ce57e246
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-08-18 22:36:12 +0530

    Update Bubble Sort.cpp

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -27,3 +27,32 @@ int main()
 
 // Time Complexity -> O(n^2)
 // Space Complexity -> O(1)
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[] = {9,12,3,1,987,0,777,43};
+     int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n-1;++i)
+        bool swapped=false;
+    {
+        for(int j=0;j<n-i-1;++j)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swapped=true;
+                swap(arr[j],arr[j+1]);
+            }
+        }
+        if(swapped==false)
+        {
+            break;
+        }
+    }
+    for(int k : arr)
+    {
+        cout<< k << " ";
+    }
+    return 0;
+}
