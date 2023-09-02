661925f8429be2a4539605c1f4daa4336efbd7de
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-09-02 22:51:35 +0530

    Create Square.cpp

---
+++ Patterns/Square.cpp
@@ -0,0 +1,23 @@
+// for printing a solid * square 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int row=0;row<n;row++)
+    {
+        for(int col=0;col<n;col++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+T.C.-> O(n^2)
+S.C.-> O(1)
