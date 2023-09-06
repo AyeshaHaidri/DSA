a5ffdd1c7152f07c26ab9c788cf0e30412d8885e
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-09-06 17:22:31 +0530

    Create Inverted Pyramid.cpp

---
+++ Patterns/Inverted Pyramid.cpp
@@ -0,0 +1,28 @@
+//inverted pyramid
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int row=0;row<n;row++)
+    {
+         //for spaces
+        for(int col=0;col<row;col++)
+        {
+            cout<<" ";
+        }
+        //for stars
+        for(int col=0;col<n-row;col++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+// T.C. -> O(n^2)
+// S.C. -> O(1)
