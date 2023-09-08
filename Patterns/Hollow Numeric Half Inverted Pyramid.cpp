3647717162499678a60767f090235c79af8dabe4
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-09-08 23:12:18 +0530

    Create Hollow Numeric Half Inverted Pyramid.cpp

---
+++ Patterns/Hollow Numeric Half Inverted Pyramid.cpp
@@ -0,0 +1,35 @@
+//hollow numeric half inverted pyramid
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int row=0;row<n;row++)
+    {
+        for(int col=0;col<n-row;col++)
+        {
+            if(row==0)
+            {
+                cout<<col+1<<" ";
+            }
+            else if(col==0)   //important line
+            {
+                cout<<row+1<< " ";
+            }
+            else{
+                if(row==n-col-1)
+                {
+                    cout<<n<<" ";
+                }
+                else{
+                    cout<<"  ";
+                }
+            }
+            
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
