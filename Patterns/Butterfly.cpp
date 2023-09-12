1e6e3b8e69e5f55dd82049e05a8dfbc2383ae064
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-09-12 23:15:31 +0530

    Update Butterfly.cpp

--- Patterns/Butterfly.cpp
+++ Patterns/Butterfly.cpp
@@ -1,2 +1,19 @@
 // Butterfly Pattern
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int row=0;row<n;row++)
+    {
+        for(int col=0;col<row+1;col++)
+        {
+            cout<<*<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
