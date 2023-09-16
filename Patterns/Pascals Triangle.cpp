c51342af6a3c583c550b8b8fd8c32dbb08fa969b
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-09-16 22:32:41 +0530

    Create Pascals Triangle.cpp

---
+++ Patterns/Pascals Triangle.cpp
@@ -0,0 +1,21 @@
+  // pascals triangle
+
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+{
+     int n;
+     cin>>n;
+     for(int row= 1; row <=n; row++){
+        for(int col = 1; col < n-row+1; col++)
+            cout<<"  ";
+        int val = 1;
+        for(int col = 1; col<= row; col++){
+            cout<<val<<"   ";
+            val = val * (row- col)/col;   //important line or formula
+    }
+        cout<<endl;
+    }
+     return 0;
+}
+    
