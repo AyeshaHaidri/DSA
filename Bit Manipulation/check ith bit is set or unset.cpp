70ca0dfac6bb5ea7e2b2b838317e600a67bcf978
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-09-21 23:53:54 +0530

    Update check ith bit is set or unset.cpp

--- Bit Manipulation/check ith bit is set or unset.cpp
+++ Bit Manipulation/check ith bit is set or unset.cpp
@@ -21,21 +21,3 @@ int main()
     return 0;
 }
 
-// check if ith bit is unset or not 
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int n;
-    cin>>n; //number 
-    int i;
-    cin>>i; //ith bit
-    if(n | (1<<i))
-    {
-        cout<<"unset"<<endl;
-    }
-    else{
-        cout<<"set"<<endl;
-    }
-    return 0;
-}
