45b493a50f0fb199a5009f4d91f0fcbc59ca8bab
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-09-23 21:24:26 +0530

    Update Uppercase to lowercase.cpp

--- Bit Manipulation/Uppercase to lowercase.cpp
+++ Bit Manipulation/Uppercase to lowercase.cpp
@@ -8,3 +8,14 @@ int main()
     cout << char(c | (1<<5)) << endl;
     return 0;
 }
+
+// Another method for converting uppercase to lowercase 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char c;
+    cin>>c;
+    cout << char(c | ' ') << endl;
+    return 0;
+}
