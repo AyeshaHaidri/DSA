86557e62b43fbd8468f81e8264f1f1d70045709a
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-09-26 23:08:39 +0530

    Update set or unset ith bit.cpp

--- Bit Manipulation/set or unset ith bit.cpp
+++ Bit Manipulation/set or unset ith bit.cpp
@@ -12,17 +12,3 @@ int main()
     cout<<ans<<endl;
     return 0;
 }
-
-// unset the ith bit
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int n;
-    cin>>n;
-    int i;
-    cin>>i;
-    int ans = (n & ~(1<<i));
-    cout<<ans<<endl;
-    return 0;
-}
