9a03b29bd026f5e46a1ff26a8b6cf7a366792ebf
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2023-09-30 23:54:51 +0530

    Create Fancy pattern.cpp

---
+++ Patterns/Fancy pattern.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern(int n)
+{
+if(n == 1)
+{
+cout << "1" << endl;
+return;
+}
+for(int i = 1; i <= n; ++i)
+{
+cout << i << " ";
+}
+cout << endl;
+pattern(n-1);
+for(int i = 1; i <=n; ++i)
+{
+cout << i << " ";
+}
+cout << endl;
+}
+int main()
+{
+    int a;
+    cin>>a;
+    pattern(a);
+    return 0;
+}
