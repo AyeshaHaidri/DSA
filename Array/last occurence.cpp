f1e0de1f6f2f813a3aaa8ae61de73c8fc43f2436
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2024-03-09 21:47:46 +0530

    Create last occurence.cpp

---
+++ Array/last occurence.cpp
@@ -0,0 +1,32 @@
+// find the last occurence of a number in sorted array
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v={10,20,20,30,30,30,30,40,40,50};
+    int target=40;
+    int n=v.size();
+    int h=n-1;
+    int l=0;
+    int mid=(l+h)/2;
+    int ans=-1;
+    while(l<=h)
+    {
+        if(v[mid]==target){
+            ans=mid;
+            l=mid+1;
+        }
+        else if(v[mid]>target)
+        {
+            h=mid-1;
+        }
+        else if(v[mid]<target)
+        {
+            l=mid+1;
+        }
+         mid=(l+h)/2;
+    }
+    cout<<ans;
+    
+}
