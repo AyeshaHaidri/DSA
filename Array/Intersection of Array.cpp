01674f53bcb125c0a35e20d396931e103173a253
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2024-03-17 11:18:39 +0530

    Update Intersection of Array.cpp

--- Array/Intersection of Array.cpp
+++ Array/Intersection of Array.cpp
@@ -36,3 +36,77 @@ return 0;
 
 // T.C  => O(n1 * n2)
 // S.C => O(n2)
+
+
+
+//Optimized approach
+//first sorting the vectors then finding the common elements then removing the duplicates 
+// by moving the unique elements at starting and then popping the duplicate elements 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> a={4,9,5,10,3,3,3,3,5,5,5};
+    vector<int> b={9,4,9,8,4,1,2,5,3,4};
+    int n=a.size();
+    int m=b.size();
+    vector<int> c;
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    int i=0; int j=0;
+    
+    //finding common elements from two vectors
+    while(i<n && j<m)
+    {
+        if(a[i]>b[j])
+        {
+            j++;
+        }
+        else if(a[i]<b[j])
+        {
+            i++;
+        }
+        else{
+            c.push_back(a[i]);
+            i++;j++;
+        }
+    }
+    
+    //placing all the uniuqe elements in the starting
+     int s=c.size();
+    int x=1; int y=0;
+    while(x<s)
+    {
+        if(c[x]==c[y])
+        {
+            x++;
+        }
+        else{
+            c[y+1]=c[x];
+            y++;x++;
+        }
+    }
+    
+   
+    //removing the duplicates
+    for(int q=s;q>y+1;q--)
+    {
+        c.pop_back();
+    }
+  
+   int f=c.size();   //very important because after poppiing elements the size changes 
+   
+  //printing the vector
+      for(int z=0;z<f;z++)
+    {
+        cout<<c[z]<<" ";
+    }
+    
+}
+
+// TC-> O(nlogn)
+// SC-> O(n)
+
+
