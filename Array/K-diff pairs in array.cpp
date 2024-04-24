97fbbd934340b0fe0e4cffc5d39e23974acd286e
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2024-04-24 21:09:08 +0530

    Update K-diff pairs in array.cpp

--- Array/K-diff pairs in array.cpp
+++ Array/K-diff pairs in array.cpp
@@ -1,5 +1,36 @@
 // find the unique pairs having difference k 
 
+// Brute Force Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> nums={3,4,2,1,1,9,10};
+    int k=2;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        set<pair<int,int>> ans;
+        int diff=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                diff=nums[i]-nums[j];
+                if(i!=j && abs(diff)==k)
+                {
+                    ans.insert({nums[i],nums[j]});
+                }
+            }
+        }
+        cout<< ans.size();
+}
+
+// TC-> O(n^2)
+// SC-> O(n)
+
+
+//Optimized Approach
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -38,3 +69,5 @@ int main()
 
 // TC -> O(nlogn)
 // SC -> O(n)
+
+
