cfcca2d4d88ceb6255ffe326860d0e5ac20f08e8
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2024-06-01 17:14:49 +0530

    Create Longest Palindromic Substring.cpp

---
+++ Strings/Longest Palindromic Substring.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string expand(string &s,int i,int j)
+{
+    string subs;
+    while( i>=0 && j<s.length() && s[i]==s[j] )
+    {
+        subs=s.substr(i,j-i+1);  // important
+        i--;
+        j++;
+    }
+    return subs;
+}
+    string countSubstrings(string s) {
+        string ans="";
+        for(int center=0;center<s.length();center++)
+        {
+            string evenSubs=expand(s,center,center);
+            string oddSubs=expand(s,center,center+1);
+            string maxLength=evenSubs.length() > oddSubs.length() ? evenSubs : oddSubs;
+            ans= maxLength.length()>ans.length() ? maxLength : ans;
+        }
+        return ans;
+    }
+int main()
+{
+    string s="aaaa";
+    string ans=countSubstrings(s);
+    cout<<ans;
+}
