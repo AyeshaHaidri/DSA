a04ea4a5b8c74c1f56adb7cfa03b87c5b0a9483b
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2024-06-05 22:54:37 +0530

    Create Decode the message.cpp

---
+++ Strings/Decode the message.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    string decodeMessage(string key, string message) {
+
+        char mapp[280]={0};
+        char start='a';
+        string ans="";
+         
+        for(auto ch : key)
+        {
+            if(ch !=' ' && mapp[ch] == 0)
+            {
+                mapp[ch]=start;
+                start++;
+            }
+           
+        }
+        
+        for(auto var : message)
+        {
+            if(var==' ')
+            {
+                ans.push_back(' ');
+            }
+            else{
+                 ans.push_back(mapp[var]);
+            }
+        }
+        
+        return ans;
+    }
+    
+int main()
+{
+   string message="vkbs bs t suepuv";
+   string key="the quick brown fox jumps over the lazy dog";
+   string ans=decodeMessage(key,message);
+   cout<<ans;
+}
+
+// TC -> O(n)
+// SC -> O(n)
