17b6a4008a0c47499dec241911f9d4d5942e0b90
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2024-06-05 11:26:58 +0530

    Update Remove duplicates 2.cpp

--- Strings/Remove duplicates 2.cpp
+++ Strings/Remove duplicates 2.cpp
@@ -53,4 +53,43 @@ int main()
 }
 
 // TC -> O(n*k^2)
+// SC -> O(1)
+
+
+// optimized approach
+// two pointer approach
+
+#include<bits/stdc++.h>
+using namespace std;
+
+   string removeDuplicates(string s, int k)
+   {
+       int i=0;
+       int j=0;
+       vector<int> count(s.length());
+       while(j<s.length())
+       {
+           s[i]=s[j];
+           count[i]=1;
+           if(i>0 && s[i]==s[i-1])
+           {
+               count[i]+=count[i-1];
+           }
+           if(count[i]==k)
+           {
+               i-=k;
+           }
+           ++i;++j;
+       }
+       return s.substr(0,i);
+   }
+int main()
+{
+    string s="deeedbbcccbdaa";
+    int k=3;
+    string ans=removeDuplicates(s,k);
+    cout<<ans<<endl;
+}
+
+// TC -> O(n)
 // SC -> O(n)
