5610e1529feb68e76f465f0d1838fc103216b555
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2024-06-24 22:58:42 +0530

    Create Integer to Roman.cpp

---
+++ Strings/Integer to Roman.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num=3749;
+        string romans[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string ans="";
+        for(int i=0;i<13;i++)
+        {
+            while(num>=values[i])
+            {
+                ans+=romans[i];
+                num-=values[i];
+            }
+        }
+        cout<<ans;
+}
