aa3bc277ab24369da2fa9b2e8b0947770dd1a956
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2024-06-25 23:37:59 +0530

    Update String Compression.cpp

--- Strings/String Compression.cpp
+++ Strings/String Compression.cpp
@@ -26,3 +26,37 @@ int main()
         }
         cout<<ans;
 }
+
+ int compress(vector<char>& chars) {
+          if(chars.size()==1) return 1;
+        int i=0;  
+        int j=0; 
+        string ans="";
+        while(i<chars.size())
+        {
+            int count=0;
+            ans+=chars[i];
+            while(j<chars.size() && chars[i]==chars[j])
+            {
+                count++;
+                j++;
+            }
+            i=j;
+            
+            if(count>1)
+            {
+                ans+=to_string(count);
+            }
+        }
+        // emptying the chars vector
+        while(chars.size()!=0)
+        {
+            chars.pop_back();
+        }
+        // copying the ans content into chars vector 
+        for( int i=0;i<ans.size();i++)
+        {
+            chars.push_back(ans[i]);
+        }
+        return chars.size();
+    }
