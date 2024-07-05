d6b1727af6abf090b2aa9217b6f3191d48228063
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2024-07-05 23:39:55 +0530

    Update Merge Sort.cpp

--- Sorting/Merge Sort.cpp
+++ Sorting/Merge Sort.cpp
@@ -66,3 +66,64 @@ return 0;
 // Space Complexity -> O(n)
 
 // In this sorting technique vector is used for storing the values temporarily but we can also use an array instead of vector.
+
+// Optimised approach 
+
+#include<bits/stdc++.h>
+using namespace std;
+void merge(int* arr,int n,int start,int mid,int end)
+{
+     int start2 = mid + 1;
+    if (arr[mid] <= arr[start2]) {
+        return;
+    }
+    while (start <= mid && start2 <= end) {
+        if (arr[start] <= arr[start2]) {
+            start++;
+        }
+        else {
+            int value = arr[start2];
+            int index = start2;
+            while (index != start) {
+                arr[index] = arr[index - 1];
+                index--;
+            }
+            arr[start] = value;
+            start++;
+            mid++;
+            start2++;
+            
+        }
+        
+    }
+
+}
+void divide(int* arr,int n,int low,int high)
+{
+if(low >= high)  
+{
+return;
+}
+int mid = (low + high)/2;
+divide(arr,n,low,mid); 
+divide(arr,n,mid+1,high);
+merge(arr,n,low,mid,high);  
+}
+int main()
+{
+int arr[] = {7,9,1,2,8,6,5,0,-1,9,99};
+int n = 11;
+int low = 0;
+int high = n - 1;
+
+divide(arr,n,low,high);
+
+for(int e: arr)
+{
+cout << e << " ";
+}
+return 0;
+}  
+
+// Time complexity -> N(logn) 
+// Space Complexity -> O(1)
