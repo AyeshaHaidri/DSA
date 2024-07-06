2de449b554692067cd98a478525688f051bf7bad
Ayesha Fatima <93004139+AyeshaHaidri@users.noreply.github.com>
2024-07-06 12:12:03 +0530

    Update Quick Sort.cpp

--- Sorting/Quick Sort.cpp
+++ Sorting/Quick Sort.cpp
@@ -54,3 +54,46 @@ return 0;
 
 // T.C => N(log2n), worst case => O(n^2)
 // S.C => O(1)
+
+
+// another approach to quick sort
+
+void quickSort(int *arr,int low,int high)
+{
+    if(low>=high) return;
+    int i=low-1;
+    int j=low;
+    int pivot=high;
+    
+    while(j<pivot)
+    {
+        if(arr[j]<arr[pivot])
+        {
+            i++;
+            swap(arr[i],arr[j]);
+        }
+        j++;
+    }
+    i++;
+    swap(arr[i],arr[pivot]);
+    
+    quickSort(arr,low,i-1);
+    quickSort(arr,low+1,high);
+}
+
+int main()
+{
+    int arr[]={-1,-2,-2,23,1,2,3,4,5,9,8,7,6,5,4,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int low=0;
+    int high=n-1;
+    quickSort(arr,low,high);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+// T.C => N(log2n), worst case => O(n^2)
+// S.C => O(1)
